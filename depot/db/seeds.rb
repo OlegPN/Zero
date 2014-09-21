b852aa7cbd6b9012b2e009c261a4a6c3aaa79ea5
Oleg <pershinoleg90@gmail.com>
2014-09-21 19:15:00 +0400

    Shop

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+# . . .
+Product.create!(title: 'The Master and Margarita',
+description:
+%{<p>
+Mikhail Bulgakov's devastating satire of Soviet life was written during 
+the darkest period of Stalin's regime. Combining two distinct yet 
+interwoven parts—one set in ancient Jerusalem, one in contemporary 
+Moscow—the novel veers from moods of wild theatricality 
+with violent storms, vampire attacks, and a Satanic ball; 
+to such somber scenes as the meeting of Pilate and Yeshu
+</p>},
+image_url: 'Mihail_Bulgakov__Master_i_Margarita.jpg',
+price: 100.00)
+# . . .
